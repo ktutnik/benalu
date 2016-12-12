9c2fe6747a3d80be0aabe4c6cfbb1a1e024ebb1f
Ketut Sandiarsa <ktutnik@gmail.com>
2016-12-12 09:08:13 +0800

    Fixing git ignores

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+"use strict";
+
+
+var gulp        = require("gulp"),
+    tsc         = require("gulp-typescript"),
+    runSequence = require("run-sequence"),
+    mocha       = require("gulp-mocha"),
+    istanbul    = require("gulp-istanbul");
+
+//******** BUILD *************
+
+var tsProject = tsc.createProject("tsconfig.json");
+
+gulp.task("build-source", function() {
+    return gulp.src([
+        "src/**/**.ts"
+    ])
+    .pipe(tsTestProject())
+    .on("error", function (err) {
+        process.exit(1);
+    })
+    .pipe(gulp.dest("src/"));
+});
+
+var tsTestProject = tsc.createProject("tsconfig.json", {
+    declaration: false
+});
+
+gulp.task("build-test", function() {
+    return gulp.src([
+        "test/*.ts"
+    ])
+    .pipe(tsTestProject())
+    .on("error", function (err) {
+        process.exit(1);
+    })
+    .pipe(gulp.dest("test"));
+});
+
+var tsEs6 = tsc.createProject("tsconfig.json", {
+    target: "es6",
+    declaration: false
+});
+
+gulp.task("build-test-es6", function() {
+    return gulp.src([
+        "test/*.es6.ts"
+    ])
+    .pipe(tsEs6())
+    .on("error", function (err) {
+        process.exit(1);
+    })
+    .pipe(gulp.dest("test"));
+});
+
+gulp.task("build", function(cb) {
+  runSequence("build-source", "build-test", "build-test-es6", cb);
+});
+
+
+//******** TEST *************
+gulp.task("mocha", function() {
+  return gulp.src([
+      "test/**/*.js"
+    ])
+    .pipe(mocha({ui: "bdd"}))
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task("istanbul:hook", function() {
+  return gulp.src(["src/**/*.js"])
+      // Covering files
+      .pipe(istanbul())
+      // Force `require` to return covered files
+      .pipe(istanbul.hookRequire());
+});
+
+gulp.task("test", function(cb) {
+  runSequence("istanbul:hook", "mocha", cb);
+});
+
+//******** DISTRIBUTION *************
+gulp.task("dist", function() {
+  return gulp.src(["src/**/*.js", "src/**/*.d.ts"])
+    .pipe(gulp.dest("dist/"));
+});
+
+//******** DEFAULT *************
+gulp.task("default", function (cb) {
+  runSequence(
+    "build",
+    "test",
+    "dist",
+    cb);
+}); 
---
+++ test/benaluproxy.test.es6.ts
@@ -0,0 +1,156 @@
+
+import * as Benalu from "../src/benalu";
+import * as Chai from "chai";
+
+const NUMBER_RESULT = 999;
+const NEW_PROPERTY_RESULT = 8888;
+const STRING_RESULT = "THIS IS STRING_RESULT";
+
+class Stub {
+    property = NUMBER_RESULT;
+
+    getNumber() {
+        return NUMBER_RESULT;
+    }
+
+    getString() {
+        return STRING_RESULT;
+    }
+
+    getData(data) {
+        return data;
+    }
+
+    substract(a: number, b: number) {
+        return a - b;
+    }
+
+    changeProperty() {
+        this.property = NEW_PROPERTY_RESULT;
+    }
+}
+
+
+describe("BenaluProxy ES6 Build", () => {
+    it("Should create a perfect copy of prototype function", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .build();
+        let numResult = proxy.getNumber();
+        let strResult = proxy.getString();
+        let dataResult = proxy.getData(300)
+
+        Chai.expect(numResult).eq(NUMBER_RESULT);
+        Chai.expect(strResult).eq(STRING_RESULT);
+        Chai.expect(dataResult).eq(300);
+    });
+
+    it("Should create perfect copy of object", () => {
+        var object = {
+            data: "THIS IS DATA",
+            getData: function() {
+                return 300;
+            }
+        }
+        let proxy = Benalu.fromInstance(object)
+            .build();
+        let numResult = proxy.getData();
+
+        Chai.expect(numResult).eq(300);
+        Chai.expect(proxy.data).eq("THIS IS DATA");
+    });
+
+    it("Should proxy method with multiple parameters", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .build();
+
+        let result = proxy.substract(9, 2);
+
+        Chai.expect(result).eq(7);
+    });
+
+    it("Should be able to change return value from interception", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception((i) => {
+                i.proceed();
+                i.returnValue = 999;
+            })
+            .build();
+        let numResult = proxy.getNumber();
+
+        Chai.expect(numResult).eq(999);
+    });
+
+    it("Should be able to use multiple interception", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception((i) => {
+                i.proceed();
+                i.returnValue = 1;
+            })
+            .addInterception((i) => {
+                i.proceed();
+                i.returnValue = i.returnValue + 1;
+            })
+            .build();
+        let numResult = proxy.getNumber();
+
+        Chai.expect(numResult).eq(2);
+    });
+
+    it("Should be able to intercept a getter", () => {
+        let original = {
+            data: 30
+        };
+
+        let memberType;
+        let proxy = Benalu.fromInstance(original)
+            .addInterception((i) => {
+                i.returnValue = 999;
+                memberType = i.memberType;
+            })
+            .build();
+        let numResult = proxy.data;
+
+        Chai.expect(numResult).eq(999);
+        Chai.expect(memberType).eq(Benalu.MemberType.Getter);
+    });
+
+    it("Should be able to intercept a setter", () => {
+        let original = {
+            data: 30
+        };
+
+        let memberType;
+        let proxy = Benalu.fromInstance(original)
+            .addInterception((i) => {
+                memberType = i.memberType;
+            })
+            .build();
+        proxy.data = 200;
+
+        Chai.expect(memberType).eq(Benalu.MemberType.Setter);
+    });
+    
+    it("Should reflect original property change when original property internally changed", () => {
+        let original = {
+            data: 30,
+            changeData: function () {
+                this.data = 999;
+            }
+        };
+
+        let memberType;
+        let proxy = Benalu.fromInstance(original)
+            .build();
+        proxy.changeData();
+
+        Chai.expect(proxy.data).eq(999);
+    });
+});
