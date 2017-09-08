81d500292e14767b4148c2714c8be23488986fad
I Ketut Sandiarsa <ktutnik@gmail.com>
2017-09-08 12:26:49 +0800

    Use ES6 proxy as proxy generator

--- src/benalu.ts
+++ src/benalu.ts
@@ -1,90 +1,11 @@
-export type MemberType = "Method" | "Getter" | "Setter"
 
-export type Interceptor = (i: Invocation) => any
+import { BenaluBuilder } from "./benalu-builder"
 
-export abstract class Invocation {
-    memberName: string;
-    parameters: IArguments;
-    memberType: MemberType;
-    abstract proceed(): any;
+export function fromInstance<T extends object>(instance: T) {
+    return new BenaluBuilder<T>(instance);
 }
 
-class MethodInvocation extends Invocation {
-    constructor(private original: any, public memberName: string, public parameters: IArguments) {
-        super()
-        this.memberType = "Method"
-    }
-
-    proceed() {
-        return this.original[this.memberName].apply(this.original, this.parameters)
-    }
-}
-
-class InterceptorInvocation extends Invocation {
-    constructor(private next: Invocation, private interceptor: Interceptor) {
-        super();
-        this.memberName = next.memberName;
-        this.memberType = next.memberType;
-        this.parameters = next.parameters;
-    }
-
-    proceed() {
-        return this.interceptor(this.next)
-    }
-}
-
-
-export function getMembers(origin) {
-    let members = Object.keys(origin);
-    let properties = Object.getOwnPropertyNames(Object.getPrototypeOf(origin));
-    members = members.concat(properties);
-    let conIndex = members.indexOf("constructor");
-    members.splice(conIndex, 1);
-    return members;
-}
-
-export class BenaluBuilder<T> {
-    origin: any;
-    intercepts: Array<Interceptor> = [];
-
-    constructor(origin) {
-        this.origin = origin
-    }
-
-    addInterception(interception: Interceptor) {
-        this.intercepts.push(interception);
-        return this;
-    }
-
-    private methodInterception(origin:any, method:string, interceptors:Interceptor[]){
-        return function(){
-            let invocation:Invocation = new MethodInvocation(origin, method, arguments)
-            interceptors.forEach(x => {
-                invocation = new InterceptorInvocation(invocation, x)
-            })
-            return invocation.proceed();
-        }
-    }
-
-    build(): T {
-        let proxy = {};
-        let members = getMembers(this.origin);
-        for (let key of members) {
-            if(typeof this.origin[key] == "function"){
-                proxy[key] = this.methodInterception(this.origin, key, this.intercepts.reverse())
-            }
-            else {
-                proxy[key] = this.origin[key]
-            }
-        }
-        return <T>proxy;
-    }
-}
-
-export function fromInstance<T>(instance: T) {
-    let config = new BenaluBuilder<T>(instance);
-    return config;
-}
+export { Invocation, Builder, Interceptor } from "./core"
 
 
 
--- test/benaluproxy.test.ts
+++ test/benaluproxy.test.ts
@@ -2,81 +2,91 @@
 import * as Benalu from "../src/benalu";
 import * as Chai from "chai";
 
-const NUMBER_RESULT = 999;
-const NEW_PROPERTY_RESULT = 8888;
-const STRING_RESULT = "THIS IS STRING_RESULT";
-
 class Stub {
-    property = NUMBER_RESULT;
+    myProp:number = 100;
 
-    getNumber() {
-        return NUMBER_RESULT;
+    getNumber(){
+        return 100;
     }
 
-    getString() {
-        return STRING_RESULT;
-    }
-
-    getData(data) {
-        return data;
-    }
-
-    substract(a: number, b: number) {
-        return a - b;
-    }
-
-    changeProperty() {
-        this.property = NEW_PROPERTY_RESULT;
+    withPar(a, b, c){
+        return [a, b, c]
     }
 }
 
-
 describe("BenaluProxy", () => {
-    it("Should create a perfect copy of prototype function", () => {
+
+    it("Should not error when no interceptor provided", () => {
         let stub = new Stub();
 
         let proxy = Benalu.fromInstance(stub)
             .build();
         let numResult = proxy.getNumber();
-        let strResult = proxy.getString();
-        let dataResult = proxy.getData(300)
 
-        Chai.expect(numResult).eq(NUMBER_RESULT);
-        Chai.expect(strResult).eq(STRING_RESULT);
-        Chai.expect(dataResult).eq(300);
+        Chai.expect(numResult).eq(100);
     });
 
-    it("Should create perfect copy of object", () => {
-        var object = {
-            data: "THIS IS DATA",
-            getData: function () {
-                return 300;
-            }
-        }
-        let proxy = Benalu.fromInstance(object)
-            .build();
-        let numResult = proxy.getData();
-
-        Chai.expect(numResult).eq(300);
-        Chai.expect(proxy.data).eq("THIS IS DATA");
-    });
-
-    it("Should proxy method with multiple parameters", () => {
+    it("Should able to intercept method properly", () => {
         let stub = new Stub();
 
         let proxy = Benalu.fromInstance(stub)
+            .addInterception(i => i.proceed())
             .build();
+        let numResult = proxy.getNumber();
 
-        let result = proxy.substract(9, 2);
+        Chai.expect(numResult).eq(100);
+    });
 
-        Chai.expect(result).eq(7);
+    it("Should give correct information about intercepted method", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception(i => {
+                Chai.expect(i.memberName).eq("withPar")
+                Chai.expect(i.memberType).eq("method")
+                Chai.expect(i.parameters[0]).eq(1)
+                Chai.expect(i.parameters[1]).eq(2)
+                Chai.expect(i.parameters[2]).eq(3)
+                return i.proceed()
+            })
+            .build();
+        let numResult = proxy.withPar(1,2,3);
+
+        Chai.expect(numResult).deep.eq([1, 2, 3])
+    });
+
+    it("Should able to intercept getter properly", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception(i => i.proceed())
+            .build();
+        let numResult = proxy.myProp;
+
+        Chai.expect(numResult).eq(100);
+    });
+
+    it("Should give correct information about intercepted getter", () => {
+        let stub = new Stub();
+
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception(i => {
+                Chai.expect(i.memberName).eq("myProp")
+                Chai.expect(i.memberType).eq("getter")
+                Chai.expect(i.parameters).undefined
+                return i.proceed()
+            })
+            .build();
+        let numResult = proxy.myProp
+
+        Chai.expect(numResult).deep.eq(100)
     });
 
     it("Should be able to change return value from interception", () => {
         let stub = new Stub();
 
         let proxy = Benalu.fromInstance(stub)
-            .addInterception((i) => 999)
+            .addInterception(i => 999)
             .build();
         let numResult = proxy.getNumber();
 
@@ -87,58 +97,38 @@ describe("BenaluProxy", () => {
         let stub = new Stub();
 
         let proxy = Benalu.fromInstance(stub)
-            .addInterception((i) => i.proceed() + 1)
-            .addInterception((i) => 1)
+            .addInterception(i => i.proceed() + 1)
+            .addInterception(i => 1)
             .build();
+
         let numResult = proxy.getNumber();
 
         Chai.expect(numResult).eq(2);
     });
 
-    /*it("Should be able to intercept a getter", () => {
-        let original = {
-            data: 30
-        };
+    it("Should execute first interceptor first", () => {
+        let stub = new Stub();
+        let order = []
 
-        let memberType;
-        let proxy = Benalu.fromInstance(original)
-            .addInterception((i) => 999)
-            .build();
-        let numResult = proxy.data;
-
-        Chai.expect(numResult).eq(999);
-        Chai.expect(memberType).eq(Benalu.MemberType.Getter);
-    });
-
-    it("Should be able to intercept a setter", () => {
-        let original = {
-            data: 30
-        };
-
-        let memberType;
-        let proxy = Benalu.fromInstance(original)
-            .addInterception((i) => {
-                memberType = i.memberType;
+        let proxy = Benalu.fromInstance(stub)
+            .addInterception(i => {
+                order.push(1)
+                return i.proceed()
+            })
+            .addInterception(i => {
+                order.push(2)
+                return i.proceed()
+            })
+            .addInterception(i => {
+                order.push(3)
+                return i.proceed()
             })
             .build();
-        proxy.data = 200;
 
-        Chai.expect(memberType).eq(Benalu.MemberType.Setter);
+        let numResult = proxy.getNumber();
+
+        Chai.expect(order).deep.eq([1, 2, 3])
+        Chai.expect(numResult).eq(100);
     });
-    
-    it("Should reflect original property change when original property internally changed", () => {
-        let original = {
-            data: 30,
-            changeData: function () {
-                this.data = 999;
-            }
-        };
 
-        let memberType;
-        let proxy = Benalu.fromInstance(original)
-            .build();
-        proxy.changeData();
-
-        Chai.expect(proxy.data).eq(999);
-    });*/
 });
